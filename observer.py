2c3b917bf69b3c129b07118580e79c17a5585143
Akifumi Takahashi <aktk.j.uec@gmail.com>
2020-12-24 02:09:09 +0900

    Modifed a sample code

--- observer.py
+++ observer.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     f1 = receive_notification_by(print,'write','post',True)
     f2 = receive_notification_by(print,'read','pre',True)
     print(f1)
-    f1('write','post',0)    # to be executed
-    f2('read','pre',2)      # to be exectuted
-    f2('read','post',4)     # not to be exectuted
+    f1(0, 'write','post')    # to be executed
+    f2(1, 'read','pre')      # to be exectuted
+    f2(2, 'read','post')     # not to be exectuted
+    f1(3, 'read','post')     # not to be exectuted
